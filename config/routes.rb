ea9a1598a0bf453451c5e452ef95fe5c5550ce9b
RachDerra <abdoulrachidederra@gmail.com>
2022-06-15 12:43:32 +0000

    Init

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :blogs
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+  post "/graphql", to: "graphql#execute"
+  resources :blogs
+  root "blogs#index"
+end
